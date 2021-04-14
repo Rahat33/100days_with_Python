fc7e3c5adfe99387e1ea693340c06e37acb2079a
Rahat Al Amin <rahatalamin33@gmail.com>
2021-04-14 16:14:23 +0600

    line shorten

--- input&len,1.py
+++ input&len,1.py
@@ -1,2 +1,4 @@
-name = input("Enter words to count characters : ")
-print(len(name))
+# name = input("Enter words to count characters : ")
+# print(len(name))
+
+print(len (input ("Enter Words to count : ")))
