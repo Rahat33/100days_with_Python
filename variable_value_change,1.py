393bfd205713103fcee5d207f431f64d42d3f7a6
Rahat Al Amin <rahatalamin33@gmail.com>
2021-04-15 16:34:04 +0600

    value_changing in variable

---
+++ variable_value_change,1.py
@@ -0,0 +1,22 @@
+# 🚨 Don't change the code below 👇
+milk = input("a: ")
+coffee = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+a = milk
+
+milk = coffee
+
+coffee = a
+
+
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + milk)
+print("b: " + coffee)
